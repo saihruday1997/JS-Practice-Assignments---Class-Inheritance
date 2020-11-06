1b6df1495eb09c0a8cf665142dea3c534d036eaa
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-11-07 01:09:13 +0530

    changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,13 @@
 // import using require
 
+const { default: shape } = require("./shape");
+
 // declare class
 
-
 // export class using module.exports
+
+export default class circle extends shape {
+  calculateArea = () => {
+    console.log("circle calc area");
+  };
+}
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
 
 // export class using module.exports
+
+export default class shape {
+  drawShape = () => {
+    console.log("draw shape");
+  };
+
+  calculateArea = () => {
+    console.log("shape calc area");
+  };
+
+  color = "blue";
+}
