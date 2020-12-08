6f257efadd50f70154495df6665fdd7147328834
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-12-09 00:04:47 +0530

    chngs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Shape from "./components/shape";
+import App from "./components/App";
 
-ReactDOM.render(<Shape />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
